a103800adb3ecfb35139d3c6ab5b371fd61f5a7d
nhoang0925 <nhoang0925@gmail.com>
2022-09-09 17:56:58 -0500

    Add Homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-
+import { Fragment } from "react";
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
 
 function App() {
-  return (
-    <div>
-     <header>Welcome to Coogs Cafe</header>
-    </div>
-  );
+    return (
+        <div>
+        <header>
+            <NavBar />
+            <Routes>
+                <Fragment><Route path="/" element={<Home />} /></Fragment>
+            </Routes>
+        </header>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to Coogs Cafe</h1>
+        </div>
+    )
+}
+
+export default Home
