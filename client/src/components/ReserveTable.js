e81533e69c880b39be51bede95ff77d3fe819967
nhoang0925 <nhoang0925@gmail.com>
2022-11-12 02:58:10 -0600

    Add working `reserve` function
    Query and backend routing works for `search` function but variables are still being hardcoded at front end level (unable to get value from front end)

--- client/src/components/ReserveTable.js
+++ client/src/components/ReserveTable.js
@@ -17,234 +17,32 @@ import TableRow from "@mui/material/TableRow";
 
 const ReserveTable = () => {
 
+    const url = 'http://localhost:5000/api/reserve'
 
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    /*
-    const [capacity, setCapacity] = useState(0);
-    useEffect(() => {
-        axios({
-            method: "GET",
-            withCredentials: false,
-        }).then((res) => {
-            setCapacity(res.data.capacity);
-            getSearch();
-            getReserves();                            
-        })
-    }, [])
-    */
-
-    /*
-    const [capacity, setCapacity] = useState(0);
-    const getTable = () => {
-        axios
-            .get(`/api/getTable`, {
-                params: {capacity : capacity}
-            })
-            .then((res) => {
-                setCapacity(res.data);
-            })
-    }
-    */
-
-
-
-    const [reserves, setReserves] = useState(0);
-
-    const getReserves = () => {
-        axios
-            .get(`/api/reserve`, {
-                params: { capacity : capacity },
-                withCredentials: false,
-                //USE user_id state for id to query
-            })
-            .then((res) => {
-                console.log(res);
-                setReserves(res.data);
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    };
-
-    //console.log(reserves);
-
-
-
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    const [totalTables, setTotalTables] = useState([]);
-
-    const [selection, setSelection] = useState({
-      table: {
-        name: null,
-        id: null
-      },
-      date: new Date(),
-      time: null,
-      capacity: 0
-    });
-  
-    const [reserving, setReserving] = useState({
-      name: "",
-      phone: "",
-      email: ""
-    });
-  
-    const [times] = useState([
-      "11AM",
-      "12PM",
-      "1PM",
-      "2PM",
-      "3PM",
-      "4PM",
-      "5PM",
-      "6PM",
-      "7PM",
-      "8PM",
-      "9PM"
-    ]);
-  
-    const [reservationError, setReservationError] = useState(false);
-  
-    const getDate = _ => {
-      const months = [
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December"
-      ];
-      const date =
-        months[selection.date.getMonth()] +
-        " " +
-        selection.date.getDate() +
-        " " +
-        selection.date.getFullYear();
-      let time = selection.time.slice(0, -2);
-      time = selection.time > 12 ? time + 12 + ":00" : time + ":00";
-      console.log(time);
-      const datetime = new Date(date + " " + time);
-      return datetime;
-    };
-
-
-    /*
-    o	User enters name, phone, email, date and time (date picker), and # of guests for dining and system presents available tables.
-    o	Tables have maximum capacity limit i.e., 2, 4, 6, or 8.
-    o	Different combinations are allowed, and owner accommodates the seating, for example: someone requests 8 guests and table for 8 is not available but 2 + 6, or 4+4 is available. 
-    System should combine the tables and notify owner they need to combine tables. In this case System reserves both tables.
-    */
-
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-    const url = 'http://localhost:5000'
-    const [data, setData]= useState({
-    capacity:""
+    const [data, setData] = useState({
+        name:"",
+        phone:"",
+        email:"",
+        date: "",
+        time: "",
+        capacity: "",
+        table:"" 
     })
 
-
-    /*
-    const [capacity, setCapacity] = useState(0);
-    const getCapacity = () => {
-        axios
-            .post(`/api/search`, {
-                params: { capacity : capacity }
-            })
-            .then((res) => {
-                //setCapacity(res.data.capacity);
-                console.log(res);
-                setCapacity(res.data);
-                //if(res.data[res.data.length - 1].state==="Texas"){
-                //    setIsInState(true);
-                //}
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    };
-
-    console.log(capacity);
-    */
     
-
-    //const [capacity, setCapacity] = useState(0);
+    var date;
+    var time;
     var capacity;
-    function handle(e){
-        const newData = {...data}
-        //newData[e.target.capacity] = e.target.value
-        //newData['date'] = date
-        //newData['time'] = time
-        newData['capacity'] = e.target.capacity 
-        newData['capacity'] = e.target.value   
-        setData(newData)
-        console.log(newData);
-        capacity = e.target.value
-        getSearch();
-    }
-    
-    function submit(e){
-        alert("Successfully Submitted");
-        e.preventDefault();
-        axios.post(url,{
-            name: data.name,
-            phone: data.phone,
-            email: data.email,
-            date: data.date,
-            time: data.time
-       })
-        .then(res=>{
-            console.log(res.data);
-        })
-    }
 
-
-    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    /*
-    const [capacity, setCapacity] = useState(0);
-    useEffect(() => {
-        axios({
-            method: "POST",
-            url: "http://localhost:5000/api/search"
-        }).then((res) => {
-            console.log(res.data.capacity)
-            if (res.data.capacity > 0) {
-                setCapacity(res.data);
-                //getSearch();
-            }
-        })
-    }, [])
-    */
-
-    var status = "";
     const [searches, setSearches]= useState(0);
-    const [isAvailable, setIsAvailable] = useState(0);
     const getSearch = () => {
         axios
             .get(`/api/getTable`, {
-                params: { capacity : capacity }
+                params: { date : date , time : time , capacity : capacity }
             })
             .then((res) => {
-                //setCapacity(res.data.capacity);
                 console.log(res);
                 setSearches(res.data);
-                /*
-                if(res.data[res.data.length - 1].isAvailable==="1"){
-                    setIsAvailable(true);                   
-                    if (setIsAvailable) {
-                        status = "Available";
-                    }
-                }
-                */
             })
             .catch((err) => {
                 console.log(err);
@@ -255,12 +53,36 @@ const ReserveTable = () => {
 
 
 
+    function handle(e){
 
+        const newData = {...data}
+        newData[e.target.id] = e.target.value
 
+        setData(newData)
+        console.log(newData)
+        date = 20221111
+        time = 200000
+        capacity = 8
+        getSearch();
+    }
+        
 
-
-    ///////////////////////////////////////////////////////////////////////////////////////////////////
-
+    function submit(e){
+        alert("Successfully Reserved");
+        e.preventDefault();
+        axios.post(url,{
+            name: data.name,
+            phone: data.phone,
+            email: data.email,
+            date: data.date,
+            time: data.time,
+            capacity: data.capacity,
+            table: data.table
+       })
+        .then(res=>{
+            console.log(res.data);
+        })
+    };
 
 
     return(
@@ -268,47 +90,49 @@ const ReserveTable = () => {
         <h1><Typography align="left" style={{color:'red', fontSize: 50}}>Make a Reservation</Typography></h1>
         <Typography align="center" style={{paddingTop: "30px"}}></Typography>
         <h4>&nbsp;Search for a Table</h4>
-        <form>
+        <form onSubmit={(e)=> submit(e)}>
             <div><Typography align="left" style={{paddingTop: "5px" }}>
                 Name &emsp;&emsp;&emsp;&emsp;&emsp;&ensp;         
-                <input type = "String" style={{fontSize: 15}} />
+                <input type = "String" style={{fontSize: 15}} onChange={(e)=>handle(e)} id="name" value={data.name}/>
             </Typography></div>
 
             <div><Typography align="left" style={{paddingTop: "10px" }}>
                 Phone Number &emsp;&ensp;
-                <input type = "tel" style={{fontSize: 15}} />
+                <input type = "tel" style={{fontSize: 15}} onChange={(e)=>handle(e)} id="phone" value={data.phone}/>
             </Typography></div>
 
             <div><Typography align="left" style={{paddingTop: "10px" }}>
                 Email &emsp;&emsp;&emsp;&emsp;&emsp;&ensp;
-                <input type = "email" style={{fontSize: 15}} />
+                <input type = "email" style={{fontSize: 15}} onChange={(e)=>handle(e)} id="email" value={data.email}/>
             </Typography></div>
 
             <div><Typography align="left" style={{paddingTop: "10px" }}>
                 Reservation Date &ensp;
-                <input type="date" id="date"></input>
+                <input type="date" id="date" name="date" onChange={(e)=>handle(e)} value={data.date}></input>
             </Typography></div> 
 
             <div><Typography align="left" style={{paddingTop: "10px" }}>
                 Reservation Time &nbsp;
-                <select id="time" name="time">
-                    <option value = "11AM">11AM</option>
-                    <option value = "12PM">12PM</option>
-                    <option value = "1PM">1PM</option>
-                    <option value = "2PM">2PM</option>
-                    <option value = "3PM">3PM</option>
-                    <option value = "4PM">4PM</option>
-                    <option value = "5PM">5PM</option>
-                    <option value = "6PM">6PM</option>
-                    <option value = "7PM">7PM</option>
-                    <option value = "8PM">8PM</option>
-                    <option value = "9PM">9PM</option>
+                <select id="time" name="time" onChange={(e)=>handle(e)} value={data.time}>
+                    <option value = ""></option>
+                    <option value = "110000">11AM</option>
+                    <option value = "120000">12PM</option>
+                    <option value = "130000">1PM</option>
+                    <option value = "140000">2PM</option>
+                    <option value = "150000">3PM</option>
+                    <option value = "160000">4PM</option>
+                    <option value = "170000">5PM</option>
+                    <option value = "180000">6PM</option>
+                    <option value = "190000">7PM</option>
+                    <option value = "200000">8PM</option>
+                    <option value = "210000">9PM</option>
                 </select>
             </Typography></div>
 
             <div><Typography align="left" style={{paddingTop: "10px" }}>
                 Party Size &emsp;&emsp;&emsp;&ensp;
                 <select id="capacity" name="capacity" onChange={(e)=>handle(e)} value={data.capacity}>
+                    <option value = ""></option>
                     <option value = "2">2</option>
                     <option value = "4">4</option>
                     <option value = "6">6</option>
@@ -332,19 +156,15 @@ const ReserveTable = () => {
                                         <Table aria-label="simple table">
                                             <TableHead>
                                                 <TableRow>
-                                                    <TableCell align="left" style={{backgroundColor: '#696773', color: 'white'}}>Table ID</TableCell>
+                                                    <TableCell align="left" style={{backgroundColor: '#696773', color: 'white'}}>Table Number</TableCell>
                                                     <TableCell align="left" style={{backgroundColor: '#696773', color: 'white'}}>Capacity</TableCell>
-                                                    <TableCell align="left" style={{backgroundColor: '#696773', color: 'white'}}>isAvailable</TableCell>
-                                                    <TableCell align="left" style={{backgroundColor: '#696773', color: 'white'}}>Status</TableCell>
                                                 </TableRow>
                                             </TableHead>
                                             <TableBody>
                                                 {searches.map((search) => (
                                                     <TableRow key={search.table_id}>
                                                         <TableCell align="left" component="th" scope="row">{"#" + search.table_id}</TableCell>
-                                                        <TableCell align="left">{search.capacity}</TableCell>
-                                                        <TableCell align="left">{search.isAvailable}</TableCell>
-                                                        <TableCell align="left">{search.status}</TableCell>                                                                                       
+                                                        <TableCell align="left">{search.capacity}</TableCell>                                                                                
                                                     </TableRow>
                                                 ))}
                                             </TableBody>
@@ -357,9 +177,27 @@ const ReserveTable = () => {
                             }
                         </>
             </div>
-        }
+        }        
+        </form>
+        <form onSubmit={(e)=> submit(e)}>
+        <div><Typography align="left" style={{paddingTop: "10px" }}>
+                Table &emsp;&emsp;&emsp;&ensp;
+                <select id="table" name="table" onChange={(e)=>handle(e)} value={data.table}>
+                    <option value = ""></option>
+                    <option value = "1">1</option>
+                    <option value = "2">2</option>
+                    <option value = "3">3</option>
+                    <option value = "4">4</option>
+                    <option value = "5">5</option>
+                    <option value = "6">6</option>
+                    <option value = "7">7</option>
+                    <option value = "8">8</option>
+                    <option value = "9">9</option>
+                </select>              
+            </Typography></div>
+
             <Typography align="center" style={{paddingTop: "30px" }}></Typography>
-            &nbsp;<button type="submit"><Typography align="center">Reserve</Typography></button>           
+            &nbsp;<button type="submit"><Typography align="center">Reserve</Typography></button>   
         </form>
     </div>
     )
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express")
 const app = express()
 const cors = require('cors');
+var multer = require('multer');
+var upload = multer(); 
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 var db = require("./database.js");
-const e = require("express");
 const db_users = 'dbreservetable.users';
 
 
@@ -11,17 +14,24 @@ app.use(cors({
     origin: "http://localhost:3000",
     methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD']
 }));
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(upload.array());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true })); 
 
 app.get('/api/getTable',(req,res)=>{
-
+ 
+    let date = req.query.date;
+    let time = req.query.time;
     let capacity = req.query.capacity;
 
-    let sql = "SELECT tables.table_id, capacity, isAvailable FROM tables WHERE tables.capacity=? AND tables.isAvailable=1";
+    //let sql = "SELECT tables.table_id, capacity, isAvailable FROM tables WHERE tables.capacity=? AND tables.isAvailable=1";
+
+    let sql = "SELECT table_id, capacity FROM tables WHERE NOT EXISTS (SELECT 1 FROM reservations WHERE reservations.table_id = tables.table_id AND reservations.date=? AND reservations.time=?) AND tables.capacity=?";
   
     let response = {};
-    db.query(sql, [capacity], (error, result) => {
+    db.query(sql, [date, time, capacity], (error, result) => {
       if (error) throw error;
       response = JSON.parse(JSON.stringify(result));
       return res.send(response);
@@ -33,18 +43,22 @@ app.post('/api/reserve',(req,res)=>{
     const name = req.body.name;
     const phone = req.body.phone;
     const email = req.body.email;
+    const date = req.body.date;
+    const time = req.body.time;
+    const capacity = req.body.capacity;
+    const table = req.body.table;
 
     console.log(name)
     console.log(phone)
-    console.log(email);
-
-
-    const capacity = req.body.capacity;
+    console.log(email)
+    console.log(date)
+    console.log(time)
     console.log(capacity)
+    console.log(table);
 
     
-    const sqlInsert = "INSERT INTO dbreservetable.reservations (name, phone, email, date, time, capacity) VALUES (?,?,?,?,?,?)";
-    db.query(sqlInsert,[name, phone, email, date, time, capacity],(err,result)=>{
+    const sqlInsert = `INSERT INTO dbreservetable.reservations (name, phone, email, date, time, capacity, table_id) VALUES ('${req.body.name}','${req.body.phone}','${req.body.email}','${req.body.date}','${req.body.time}', '${req.body.capacity}','${req.body.table}')`;
+    db.query(sqlInsert,(err,result)=>{
         console.log(result);
     });
 });
